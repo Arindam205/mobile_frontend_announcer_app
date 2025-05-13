d36b148c174502856ca9ddc2f69015d2bc0238ca
Arindam <paul.arindam205@gmail.com>
2025-05-13 20:58:39 +0530

    need some changes

--- app.config.js
+++ app.config.js
@@ -1,4 +1,3 @@
-// app.config.js
 module.exports = {
   expo: {
     name: "RAISE",
@@ -9,26 +8,17 @@ module.exports = {
     userInterfaceStyle: "automatic",
     owner: "arindam1996",
     
-    // Keep minimal Android config
     android: {
       package: "com.yourcompany.announceapp",
       versionCode: 1,
+      permissions: ["INTERNET"]
     },
     
-    // Use config plugins to manage native configuration
     plugins: [
       "expo-router",
-      "expo-secure-store",
-      [
-        "expo-build-properties",
-        {
-          "android": {
-            "usesCleartextTraffic": true,
-            "networkSecurityConfigPath": "./network-security-config.xml"
-          }
-        }
-      ]
+      "expo-secure-store"
     ],
+    
     extra: {
       eas: {
         projectId: "ffb88d52-5601-4488-9523-56b3ffcc99ed"
